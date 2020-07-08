d9bbefbfc6e200b0788e01046f6eb9fa08f84255
Eric Harar <erez_harari@hotmail.com>
2020-07-08 15:09:15 -0400

    Support click, double click, enhanced config

--- switch_actions.py
+++ switch_actions.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# example gpiozero code that could be used to have a reboot
-#  and a shutdown function on one GPIO button
+# eric-h-st - 2020-07
 # scruss - 2017-10
 
-use_button=27                       # lowest button on PiTFT+
-
 import os
+import threading
 from gpiozero import Button
 from signal import pause
 from subprocess import check_call
 from pygame import mixer
 import json
+import math
 
-print('Started')
 path=os.path.dirname(os.path.realpath(__file__))
 
 with open(path+'/config.json', 'r') as configFile:
     config = json.load(configFile);
 
-actions = config['actions']
+hardwareBCMPin = config["hardwareBCMPin"]
+if (hardwareBCMPin is None):
+    hardwareBCMPin = 27
+
+actions = config["timedActions"]
 if (actions is None):
     exit('Error reading config: actions is mandatory')
 
@@ -35,37 +36,74 @@ for action in actions:
     else:
         action['soundModule'] = None
 
-held_for=0.0
-resetAt=5
-shutdownAt=10
-global selectedAction 
+global selectedAction
+global clickMode
+global timer
+global lastClickSoundTime
 selectedAction = None
+clickMode = 0
+timer = None
+lastClickSoundTime = 0
 
 def rls():
         global selectedAction
-        if (selectedAction is not None):
-            os.system(selectedAction)
-        selectedAction = None
+        global clickMode
+        global timer
+        global lastClickSoundTime
+
+        lastClickSoundTime = 0
+        if (clickMode == 0):
+            if (selectedAction is not None):
+                print("Performing: ", action["name"])
+                os.system(selectedAction)
+            selectedAction = None
+        elif (clickMode == 1):
+            if (timer is not None):
+                clickMode = 2
+            else:
+                print("Starting click timer")
+                timer=threading.Timer(0.250, actOnClicks)
+                timer.start()
+
+def actOnClicks():
+       global timer
+       timer.cancel()
+       timer = None
+       print("Click timer up, clickMode=", clickMode)
+       if (clickMode == 1 and config["clickAction"] is not None):
+           os.system(config["clickAction"])
+       elif (clickMode == 2 and config["doubleClickAction"] is not None):
+           os.system(config["doubleClickAction"])
 
 def hld():
         global selectedAction
+        global clickMode
+        global lastClickSoundTime
         # callback for when button is held
         #  is called every hold_time seconds
         held_for = button.held_time + button.hold_time
 
-        actionFound = False
-        for action in actions:
-            actionElapsedSeconds = action["elapsedSeconds"]
-            if (held_for > actionElapsedSeconds  and held_for < actionElapsedSeconds  + 1):
-                if (action["soundModule"] is not None):
-                    action["soundModule"].play(0,900)
-                if (action["action"] is not None):
-                    selectedAction = action["action"]
-                    actionFound = True
-        if (not actionFound and clickSound is not None):
-            clickSound.play(0,900)
+        if (held_for > 0.05 and held_for < 0.250):
+            clickMode=1
+        else:
+            clickMode = 0
+            actionFound = False
+            for action in actions:
+                actionElapsedSeconds = action["elapsedSeconds"]
+                if (held_for > actionElapsedSeconds  and held_for < actionElapsedSeconds  + 1):
+                    if (action["soundModule"] is not None):
+                        action["soundModule"].play(0,900)
+                    if (action["action"] is not None):
+                        selectedAction = action["action"]
+                        actionFound = True
+            if (held_for > lastClickSoundTime + 1):
+                lastClickSoundTime = math.floor(held_for)
+                print(lastClickSoundTime, held_for)
+                if (not actionFound and clickSound is not None):
+                    print("Beep")
+                    clickSound.play(0,900)
 
-button=Button(use_button, hold_time=1.0, hold_repeat=True)
+button=Button(hardwareBCMPin, hold_time=0.050, hold_repeat=True)
 button.when_held = hld
 button.when_released = rls
 
