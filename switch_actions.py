3aec59f977b2699488aaba26f6425d3a51f2f315
Eric Harar <erez_harari@hotmail.com>
2020-07-08 16:21:42 -0400

    Efficiency

--- switch_actions.py
+++ switch_actions.py
@@ -40,28 +40,31 @@ global selectedAction
 global clickMode
 global timer
 global lastClickSoundTime
+global actionNum
+
 selectedAction = None
 clickMode = 0
 timer = None
 lastClickSoundTime = 0
+actionNum = 0
 
-def rls():
+def actOnButtonRelease():
         global selectedAction
         global clickMode
         global timer
         global lastClickSoundTime
+        global actionNum
 
         lastClickSoundTime = 0
         if (clickMode == 0):
             if (selectedAction is not None):
-                print("Performing: ", action["name"])
                 os.system(selectedAction)
             selectedAction = None
+            actionNum = 0
         elif (clickMode == 1):
             if (timer is not None):
                 clickMode = 2
             else:
-                print("Starting click timer")
                 timer=threading.Timer(0.250, actOnClicks)
                 timer.start()
 
@@ -69,18 +72,17 @@ def actOnClicks():
        global timer
        timer.cancel()
        timer = None
-       print("Click timer up, clickMode=", clickMode)
        if (clickMode == 1 and config["clickAction"] is not None):
            os.system(config["clickAction"])
        elif (clickMode == 2 and config["doubleClickAction"] is not None):
            os.system(config["doubleClickAction"])
 
-def hld():
+def actOnButtonHold():
         global selectedAction
         global clickMode
         global lastClickSoundTime
-        # callback for when button is held
-        #  is called every hold_time seconds
+        global actionNum
+
         held_for = button.held_time + button.hold_time
 
         if (held_for > 0.05 and held_for < 0.250):
@@ -88,23 +90,29 @@ def hld():
         else:
             clickMode = 0
             actionFound = False
-            for action in actions:
+            if (actionNum == (len(actions))):
+                return;
+
+            action = actions[actionNum]
+            if (action["elapsedSeconds"] is None):
+                actionElapsedSeconds = 99
+            else:
                 actionElapsedSeconds = action["elapsedSeconds"]
-                if (held_for > actionElapsedSeconds  and held_for < actionElapsedSeconds  + 1):
-                    if (action["soundModule"] is not None):
-                        action["soundModule"].play(0,900)
-                    if (action["action"] is not None):
-                        selectedAction = action["action"]
-                        actionFound = True
+
+            if (held_for > actionElapsedSeconds):
+                if (action["soundModule"] is not None and selectedAction != action["action"]):
+                    action["soundModule"].play(0,900)
+                if (action["action"] is not None):
+                    selectedAction = action["action"]
+                    actionFound = True
+                actionNum = actionNum + 1
             if (held_for > lastClickSoundTime + 1):
                 lastClickSoundTime = math.floor(held_for)
-                print(lastClickSoundTime, held_for)
                 if (not actionFound and clickSound is not None):
-                    print("Beep")
                     clickSound.play(0,900)
 
 button=Button(hardwareBCMPin, hold_time=0.050, hold_repeat=True)
-button.when_held = hld
-button.when_released = rls
+button.when_held = actOnButtonHold
+button.when_released = actOnButtonRelease
 
 pause() # wait forever
